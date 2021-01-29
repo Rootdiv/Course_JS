35078b15bfdbe47d3fd5920ebacd8b99a2e2cd48
Rootdiv <rootdiv@mail.ru>
2021-01-30 00:28:44 +0300

    feat: Lesson02 Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,13 @@ console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(budgetDay);
+
+//Усложнённое задание
+const num = 266219;
+const numArr = num.toString().split('');
+let result = 1;
+for (let i = 0; i < numArr.length; i++) {
+  result *= parseInt(numArr[i]);
+}
+const degree = result ** 3;
+console.log(degree.toString().slice(0, 2));
